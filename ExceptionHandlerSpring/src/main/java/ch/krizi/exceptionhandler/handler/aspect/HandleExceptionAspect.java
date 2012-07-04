7182490a22984990014cf2efc7a1dbd6e3a05351
krizi <krizi@192.168.0.13>
2012-07-04 22:25:37 +0200

    format xmls add asserts to handleexceptionaspect

--- ExceptionHandlerSpring/src/main/java/ch/krizi/exceptionhandler/handler/aspect/HandleExceptionAspect.java
+++ ExceptionHandlerSpring/src/main/java/ch/krizi/exceptionhandler/handler/aspect/HandleExceptionAspect.java
@@ -9,6 +9,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.Assert;
 
 import ch.krizi.exceptionhandler.aspect.AbstractExceptionAspect;
 import ch.krizi.exceptionhandler.handler.ExceptionHandlerManager;
@@ -30,6 +31,9 @@ public class HandleExceptionAspect extends AbstractExceptionAspect {
 
 	@AfterThrowing(pointcut = "execution(* *(..)) && @annotation(handleException)", throwing = "exception")
 	public void afterException(JoinPoint joinpoint, Throwable exception, HandleException handleException) {
+		Assert.notNull(exception, "exception musst not be null");
+		Assert.notNull(handleException, "annotation must not be null");
+		
 		if (logger.isDebugEnabled()) {
 			logger.debug("handle Exception...");
 		}
