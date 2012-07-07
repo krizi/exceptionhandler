7cd2318e826df77d898deded0ecc92b2f6367e52
krizi <krizi@192.168.0.13>
2012-07-07 18:57:05 +0200

    add testcase

---
+++ ExceptionHandler/src/test/java/ch/krizi/exceptionhandler/handler/factory/utils/ExceptionHandlerFactoryUtilsDefaultTestCase.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package ch.krizi.exceptionhandler.handler.factory.utils;
+
+import java.util.List;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ch.krizi.exceptionhandler.handler.factory.AbstractExceptionHandlerFactory;
+
+/**
+ * @author krizi
+ * 
+ */
+public class ExceptionHandlerFactoryUtilsDefaultTestCase {
+	private ExceptionHandlerFactoryUtilsDefault exceptionHandlerFactoryUtilsDefault;
+
+	@Before
+	public void init() {
+		exceptionHandlerFactoryUtilsDefault = new ExceptionHandlerFactoryUtilsDefault();
+	}
+
+	@Test
+	public void testCreate() {
+		List<AbstractExceptionHandlerFactory> exceptionHandlerFactories = exceptionHandlerFactoryUtilsDefault
+				.getExceptionHandlerFactories();
+		Assert.assertNotNull(exceptionHandlerFactories);
+	}
+}
